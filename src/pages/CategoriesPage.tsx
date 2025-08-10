f44b6566c4fb79f77e4d955c3215a07040216b21
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-08-10 23:26:30 +0500

    fix

--- src/pages/CategoriesPage.tsx
+++ src/pages/CategoriesPage.tsx
@@ -13,7 +13,9 @@ export default function CategoriesPage() {
                             <div className="aspect-square">
                                 <img src={cat.image || "/placeholder.svg"} alt={cat.name}/>
                             </div>
-                            <div className="price">{cat.name}</div>
+                            <div className="price" style={
+                                {fontSize: "1.2rem", fontWeight: "bold", marginTop: "0.5rem"}
+                            }>{cat.name}</div>
                         </div>
                     </Link>
                 ))}
